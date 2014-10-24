2148ffb234f6131d96404a555f6ede6ff34fef27
Gustavo Barbosa <gustavocsb@gmail.com>
2014-10-24 00:02:50 -0200

    Lib creation.

---
+++ Example/Pods/Target Support Files/Pods-ZBCImageViewer/Pods-ZBCImageViewer-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ZBCImageViewer
+#define COCOAPODS_POD_AVAILABLE_ZBCImageViewer
+#define COCOAPODS_VERSION_MAJOR_ZBCImageViewer 0
+#define COCOAPODS_VERSION_MINOR_ZBCImageViewer 1
+#define COCOAPODS_VERSION_PATCH_ZBCImageViewer 0
+
